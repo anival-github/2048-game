ba39b44ca8a9afe36fc38c8c1d1ec23407c178fc
Valentin Anikin <anival-94@mail.ru>
2021-03-03 15:56:48 +0300

    feat: add back-end files

---
+++ 2048-back-end/routes/gameStatusRouter.ts
@@ -0,0 +1,37 @@
+import { v4 as uuid } from 'uuid'
+import { Router } from 'express';
+import * as storage from '../storage/mongo'
+
+const router = Router();
+
+/* GET gamestatus listing. */
+router.get('/', async (req, res, next) => {
+  try {
+    const list = await storage.listAll()
+    res.json(list)
+  } catch (e) {
+    res.status(500).json({ message: 'something went wrong. Try one more time.'})
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  const id = uuid();
+
+  const { body } = req
+  body.id = id;
+
+  const newBody = await storage.create(body);
+
+  res.json(newBody)
+});
+
+router.delete('/', async (req, res, next) => {
+
+  const result = await storage.remove({});
+
+  res
+      .status(204)
+      .json(result)
+});
+
+export default router;
